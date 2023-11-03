670ebe871a7fb4a2164485bec9c62c2f3702176e
Harshit Jain <harshitostwal1234@gmail.com>
2023-11-03 22:01:02 +0530

    Parth Studio

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,37 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import Logo from '@/public/logo/ps.png'
 import React from 'react'
+import SocailMediaIcon from './Data/SocialMedia'
 
 export default function Footer() {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <>
+            {/* Footer Contains Logo , Navigation , Socail Links , CopyRights */}
+            <div className="bg-black text-white flex flex-col justify-center gap-5 w-full h-52 xl:h-60 lg:h-46 items-center md:p-8">
+                <div>
+                    {/* Website Logo */}
+                    <Link href={"/"}>
+                        <Image
+                            src={Logo}
+                            alt='Parth Studio'
+                            quality={100}
+                            layout="responsive"
+                            loading='lazy'
+                            sizes="(max-width: 1920px) 80px, (max-width: 2600px) 120px, 140px"
+                        />
+                    </Link>
+                </div>
+
+                <h1 className="text-gray-400 font-Poppins font-semibold text-sm sm:text-base">Copyright © 2023 - All right reserved by Harshit Ostwal</h1>
+
+                {/* Social Media Icons */}
+                <div className="flex gap-2 text-slate-400 items-center justify-center">
+                    {SocailMediaIcon.map((Icons) => (
+                        <Link key={Icons.id} href={Icons.href}>{Icons.icon}</Link>
+                    ))}
+                </div>
+            </div>
+        </>
+    )
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -24,7 +24,7 @@ export default function Navbar() {
     return (
         <>
             {/* Navbar Which Contains Logo And Navigation Bar */}
-            <div className="fixed w-full h-32 bg-white shadow-lg flex p-7">
+            <div className="w-full h-32 bg-white shadow-lg flex p-7">
 
                 {/* Menu Button in the Left */}
                 <button onClick={Toggle}><Grip size={24} color='black' /></button>
@@ -36,8 +36,8 @@ export default function Navbar() {
                             src={LogoBlack}
                             alt='Parth Studio'
                             quality={100}
-                            priority
                             layout="responsive"
+                            loading='lazy'
                             sizes="(max-width: 1920px) 100px, (max-width: 2600px) 120px, 140px"
                         />
                     </Link>
@@ -51,7 +51,7 @@ export default function Navbar() {
             <div
                 //Sidebar CSS
                 className={
-                    Menu ? "fixed left-0 top-0 w-[100%] sm:w-[70%] md:w-[25%] flex flex-col transition-all duration-1000 ease-in-out p-8 h-full bg-slate-800 text-slate-400" : "fixed left-[-100%] w-[100%] sm:w-[70%] md:w-[25%] top-0 flex flex-col transition-all duration-1000 ease-in-out p-8 h-full bg-slate-800 text-slate-400"
+                    Menu ? "fixed left-0 top-0 w-[100%] sm:w-[70%] md:w-[25%] flex flex-col transition-all duration-1000 ease-in-out p-8 h-full bg-slate-800" : "fixed left-[-100%] w-[100%] sm:w-[70%] md:w-[25%] top-0 flex flex-col transition-all duration-1000 ease-in-out p-8 h-full bg-slate-800"
                 }>
 
                 <div className="flex">
@@ -62,7 +62,7 @@ export default function Navbar() {
                                 src={LogoWhite}
                                 alt='Parth Studio'
                                 quality={100}
-                                priority
+                                loading='lazy'
                                 layout="responsive"
                                 sizes="(max-width: 1920px) 80px, (max-width: 2600px) 120px, 140px"
                             />
@@ -74,12 +74,12 @@ export default function Navbar() {
                 {/* Sidebar Navigation Routes */}
                 <div className="flex flex-1 flex-col justify-start mt-20 items-center gap-4">
                     {SidebarData.map((Data) => (
-                        <Link className="font-Merienda text-2xl font-semibold" onClick={Toggle} key={Data.id} href={Data.href}>{Data.title}</Link>
+                        <Link className="font-Merienda text-slate-400 text-2xl font-semibold" onClick={Toggle} key={Data.id} href={Data.href}>{Data.title}</Link>
                     ))}
                 </div>
 
-                {/* Sidebar Socail Media Icons */}
-                <div className="flex gap-2 items-center justify-center">
+                {/* Sidebar Social Media Icons */}
+                <div className="flex gap-2 text-slate-400 items-center justify-center">
                     {SocailMediaIcon.map((Icons) => (
                         <Link key={Icons.id} href={Icons.href}>{Icons.icon}</Link>
                     ))}
