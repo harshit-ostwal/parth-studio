9dd8044654f567a1c5e38e5e774906ce3b9fe536
Harshit Jain <harshitostwal1234@gmail.com>
2023-11-07 18:53:02 +0530

    Parth Studio

--- app/Films/[id]/page.jsx
+++ app/Films/[id]/page.jsx
@@ -11,15 +11,14 @@ export default function page() {
   //It Filters Videos Data From The FilmsData PathName Href
   const filteredVideos = FilmsData.filter((video) => video.href === pathname);
 
-
   return (
     <>
       <div className="bg-slate-950 w-full h-screen top-0 absolute flex items-center flex-1 justify-center">
-        <div className="w-1/2">
+        <div className="w-[100%] lg:w-[80%] aspect-video">
 
           {/* FilterVideos Maps The Video By The Href */}
           {filteredVideos.map((data) => (
-            <iframe src={data.video} frameborder="0" className="aspect-video" width="100%" height="100%" allow="autoplay;"></iframe>
+            <iframe key={data.id} src={data.video} frameborder="0" className="aspect-video" width="100%" height="100%" allow="autoplay;"></iframe>
           ))}
         </div>
       </div>
